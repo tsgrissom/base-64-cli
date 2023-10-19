01f319fc973433a2408ff14f367f56f0955c2f87
tsgrissom <tylergrissom@me.com>
2023-10-19 16:03:38 -0500

    Added command aliases, cleaner declaration in commands.js

--- src/command.js
+++ src/command.js
@@ -4,10 +4,15 @@ import yargs from 'yargs';
 import { handleDecodeCommand, handleEncodeCommand } from './command-delegates.js'
 import { handleHistoryCommand, handleToggleCopyCommand } from './command-delegates.js'
 
-export const decodeYargs = () => yargs.positional('input', {
+// export const decodeYargs = () => yargs.positional('input', {
+//     type: 'string',
+//     description: 'An base64 encoded string to be decoded'
+// });
+
+const decodeYargs = {
     type: 'string',
-    description: 'An base64 encoded string to be decoded'
-});
+    describe: 'A base64 encoded string to be decoded'
+}
 
 export const encodeYargs = () => yargs.positional('input', {
     type: 'string',
@@ -15,25 +20,40 @@ export const encodeYargs = () => yargs.positional('input', {
 });
 
 yargs(hideBin(process.argv))
-    .command(
-        'decode <input>',
-        'Decode a base64 hash',
-        _    => decodeYargs,
-        argv => handleDecodeCommand(argv))
-    .command(
-        'encode <input>',
-        'Encode an unencoded string into base64',
-        _    => encodeYargs,
-        argv => handleEncodeCommand(argv))
-    .command(
-        'history',
-        'View the history of base-64-cli',
-        _    => {},
-        _ => handleHistoryCommand())
-    .command(
-        'copy',
-        'Toggles whether a result will be copied to system clipboard',
-        _    => {},
-        _ => handleToggleCopyCommand())
+    .command({
+        command: 'decode <input>',
+        aliases: ['dec', 'd'],
+        describe: 'Decode a base64 hash',
+        builder: {
+            input: {
+                type: 'string',
+                describe: 'A base64 encoded string to be decoded'
+            }
+        },
+        handler: argv => handleDecodeCommand(argv)
+    })
+    .command({
+        command: 'encode <input>',
+        aliases: ['enc', 'e'],
+        describe: 'Encode a string into a base64 hash',
+        builder: {
+            input: {
+                type: 'string',
+                describe: 'An unencoded string to be encoded'
+            }
+        },
+        handler: argv => handleEncodeCommand(argv)
+    })
+    .command({
+        command: 'history',
+        aliases: ['hist', 'h'],
+        describe: 'View the history of base-64-cli',
+        handler: _ => handleHistoryCommand()
+    })
+    .command({
+        command: 'copy',
+        describe: 'Toggles auto-copying to clipboard',
+        handler: _ => handleToggleCopyCommand()
+    })
     .demandCommand(1)
     .parse();
